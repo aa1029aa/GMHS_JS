92511eddf96fc3d3dce7278d36d05fcebf0d69c8
aa1029aa <ronanvieri@gmail.com>
2026-02-13 10:17:46 +0100

    3.hodina1

--- 2.lekce/script.js
+++ 2.lekce/script.js
@@ -10,3 +10,38 @@ console.log(userName)
 console.log(6+5);
 console.log("7" + "4");
 console.log("7" + 4)
+
+let age = 16;
+console.log(age > 20)
+
+let text = "TEXT";
+console.log(typeof text)
+
+let hodnota = null;
+console.log(hodnota)
+
+let cisloA = 10;
+let cisloB = 5;
+console.log(cisloA+cisloB)
+console.log(cisloA-cisloB)
+console.log(cisloA*cisloB)
+console.log(cisloA/cisloB)
+// Umocnování **
+console.log(cisloA ** cisloB)
+// Zbytek
+
+console.log(cisloA % cisloB)
+
+console.log(cisloA + cisloB * cisloB)
+
+console.log((cisloA + cisloB) * cisloB)
+// Aktualizace proměnne
+
+let number = 18;
+number = number + 1;
+number +=1 ;
+number -=1 ;
+number *=3 ;
+number /=4 ;
+
+console.log(number);
